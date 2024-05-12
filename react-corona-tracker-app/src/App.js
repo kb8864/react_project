97a4889d4c031f15a709b4d7606bde7e3790123a
kb8864 <yt7s858864@gmail.com>
2024-05-13 03:03:26 +0900

    jsxの学習、コンポーネントの階層構造で表示される内容の違いの学習

--- react-corona-tracker-app/src/App.js
+++ react-corona-tracker-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import TopPage from './pages/TopPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>おはよう</h1>
+      <h1>ようこそ</h1>
+      <TopPage />
     </div>
   );
 }
